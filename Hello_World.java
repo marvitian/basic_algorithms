533c4df64c268d8bd2a28d3daa4d681e60ec8d47
marvitian <80377652+marvitian@users.noreply.github.com>
2022-02-02 03:06:41 -0500

    the good ol' days

--- Hello_World.java
+++ Hello_World.java
@@ -1,6 +1,6 @@
 public class Hello_World{
 
 public static void main(String[] args){
-System.out.println("hello neyekowleyesdayviedaredrive");
+System.out.println("this one is just for nostalgia");
 }
 }
